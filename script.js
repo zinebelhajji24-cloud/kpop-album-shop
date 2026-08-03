10d1c80b47b0a4d28ed6e857a70856179f9b4bfb
zinebelhajji24-cloud <zinebelhajji24@gmail.com>
2026-08-03 21:56:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,27 @@ status:"instock"
 }
 ];
 
-// Firebase connection will be added here
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiZf5_fETiXKMfNSWy6ECBgH5QNWiL-CA",
+  authDomain: "kpop-album-shop.firebaseapp.com",
+  projectId: "kpop-album-shop",
+  storageBucket: "kpop-album-shop.firebasestorage.app",
+  messagingSenderId: "365809659150",
+  appId: "1:365809659150:web:c0d76829de67480216fb5f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loadAlbums() {
+  const snapshot = await getDocs(collection(db, "albums"));
+
+  snapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+}
+
+loadAlbums();
