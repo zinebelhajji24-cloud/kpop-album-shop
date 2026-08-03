81c0424a788ff81e845d7facba8e5b2984f817dd
zinebelhajji24-cloud <zinebelhajji24@gmail.com>
2026-08-03 19:07:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const albums = [];
+const albums = [
+{
+id:1,
+group:"BLACKPINK",
+album:"Born Pink",
+price:250,
+image:"images/albums/born-pink.png",
+stock:5,
+status:"instock"
+},
+{
+id:2,
+group:"BTS",
+album:"Proof",
+price:300,
+image:"images/albums/proof.png",
+stock:3,
+status:"instock"
+}
+];
