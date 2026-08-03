afa2ec78f54254c8d20fc3dff4a36c4e7b266d1c
zinebelhajji24-cloud <zinebelhajji24@gmail.com>
2026-08-03 18:59:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// ================================
+// K-pop Album Shop
+// Main JavaScript
+// ================================
 
+// Current language
+let currentLanguage = localStorage.getItem("language");
+
+// Save language
+function setLanguage(language) {
+
+    localStorage.setItem("language", language);
+
+    currentLanguage = language;
+
+}
+
+// Get language
+function getLanguage() {
+
+    return currentLanguage;
+
+}
+
+// Check first visit
+function isFirstVisit() {
+
+    return currentLanguage === null;
+
+}
+
+console.log("K-pop Album Shop Loaded");
